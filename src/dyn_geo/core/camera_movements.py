d7872de6c319e4d54a99fa4ec24f1a376431e4c6
Florent <florent.fournier@wavesnsee.com>
2026-07-09 20:25:21 +0200

    despike camera movements

--- src/dyn_geo/core/camera_movements.py
+++ src/dyn_geo/core/camera_movements.py
@@ -9,6 +9,7 @@ from georef.operators import Georef, ExtrinsicMatrix
 from dyn_geo.core import img
 from scipy.spatial.transform import Rotation as R
 from scipy.spatial.transform import Slerp
+from scipy.interpolate import make_splprep
 from matplotlib.dates import date2num
 from datetime import timedelta, datetime
 
@@ -140,41 +141,17 @@ def compute_targets_extrinsic(dir_h, f_gcps, f_cam_params, target_imgs_dir, ref_
     return date, georef_params_upd
 
 
-# def interp_targets_extrinsic(odir_cparams_upd, odir_cparams_upd_smooth):
-def interp_targets_extrinsic(dates, georef_params_upd, f_cam_params):
-    '''
-    Interpolation of rotation vector to compute smoothed camera parameters
-    '''
+def interp_targets_rvec(georef_params_upd, dates, interp_dates):
 
-
-    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.transform.Slerp.html
-    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.transform.Rotation.html#scipy.spatial.transform.Rotation
-
-    # read georef parameters, that will be updated for each target image
-    georef_params = Georef.from_param_file(f_cam_params)
-
-    # initialize variable rvecs
+    # store targets rvec to a list
     rvecs = []
-
-    def round_to_day_at_noon(dt):
-        return dt.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=0.5)
-
     for i in range(len(georef_params_upd)):
         rvecs.append(georef_params_upd[i].extrinsic.rvec.squeeze())
 
-    # Initialize the multiple rotations in one object
+    # Initialize the multiple rotations in one Rotation object
     rots = R.from_rotvec(rvecs)
 
-    # dates to perform interpolation
-    interp_dates = np.arange(round_to_day_at_noon(dates[0]), round_to_day_at_noon(dates[-1]) + timedelta(days=1),
-                             timedelta(days=1)).astype(datetime)
-    interp_dates = [date2num(interp_dates[i]) for i in range(len(interp_dates))]
-
-    # initialize list of Georef objects
-    georef_params_interp = [copy(georef_params) for i in range(len(interp_dates))]
-
     # Create the interpolator object
-    dates = [date2num(dates[i]) for i in range(len(dates))]
     slerp = Slerp(dates, rots)
 
     # perform Spherical Linear Interpolation of Rotations
@@ -183,9 +160,76 @@ def interp_targets_extrinsic(dates, georef_params_upd, f_cam_params):
     # save interp_rots as rotation vectors
     interp_rvecs = [interp_rots[i].as_rotvec() for i in range(len(interp_rots))]
 
+    return interp_rvecs
+
+
+def interp_targets_tvec(georef_params_upd, dates, interp_dates):
+
+    # store targets txn ty, tz to lists
+    tx = []
+    ty = []
+    tz = []
+    for i in range(len(georef_params_upd)):
+        tx_i, ty_i, tz_i = (georef_params_upd[i].extrinsic.tvec.squeeze())
+        tx.append(tx_i)
+        ty.append(ty_i)
+        tz.append(tz_i)
+
+    # normalize dates
+    dates = np.array(dates)
+    u = (dates - dates.min()) / (dates.max() - dates.min())
+    interp_dates = np.array(interp_dates)
+    u_interp =  (interp_dates - dates.min()) / (dates.max() - dates.min())
+
+    # Interpolate with smoothing
+    spline, u_fit = make_splprep([tx, ty, tz],
+                                 u=u,
+                                 s=5,  # smoothing parameter
+                                 k=3)  # cubic
+
+    tvec_interp = spline(u_interp)
+
+    return tvec_interp
+
+
+def interp_targets_extrinsic(dates, georef_params_upd, f_cam_params):
+    '''
+    Interpolation of rotation vector to compute smoothed camera parameters
+    '''
+
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.transform.Slerp.html
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.transform.Rotation.html#scipy.spatial.transform.Rotation
+
+    # read georef parameters, that will be updated for each target image
+    georef_params = Georef.from_param_file(f_cam_params)
+
+    def round_to_day_at_noon(dt):
+        return dt.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=0.5)
+
+    # dates to perform interpolation
+    interp_dates = np.arange(round_to_day_at_noon(dates[0]), round_to_day_at_noon(dates[-1]) + timedelta(days=1),
+                             timedelta(days=1)).astype(datetime)
+    interp_dates = interp_dates[np.logical_and((interp_dates > dates.min()), (interp_dates < dates.max()))]
+
+    # convert dates to num
+    dates = [date2num(dates[i]) for i in range(len(dates))]
+    interp_dates = [date2num(interp_dates[i]) for i in range(len(interp_dates))]
+
+    # initialize list of Georef objects
+    georef_params_interp = [copy(georef_params) for _ in range(len(interp_dates))]
+
+    # interpolation of rvec
+    interp_rvecs = interp_targets_rvec(georef_params_upd, dates, interp_dates)
+
+    # interpolation of tvec
+    interp_tvecs = interp_targets_tvec(georef_params_upd, dates, interp_dates)
+
     # save updated georef parameters
     for i in range(len(interp_rvecs)):
-        extrinsic_interp = ExtrinsicMatrix(interp_rvecs[i], georef_params_upd[0].extrinsic.tvec)
+        # if we want to leave tvec unchanged
+        # extrinsic_interp = ExtrinsicMatrix(interp_rvecs[i], georef_params_upd[0].extrinsic.tvec)
+        # otherwise:
+        extrinsic_interp = ExtrinsicMatrix(interp_rvecs[i], interp_tvecs[:, i])
         georef_params_interp[i].extrinsic = extrinsic_interp
 
     return interp_dates, georef_params_interp
@@ -216,28 +260,56 @@ def compute_cam_mvts(list_georef_params):
     return angles, position
 
 
-def plot_cam_mvts(date, angles, position, dates_interp, angles_interp, position_interp, outdir_cam_mvts):
+def despike_cam_mvts(position_ref, position, threshold_d=0.6):
+
+    # compute target's camera position distance from reference camera position
+    d = []
+    for i in range(len(position['x'])):
+        diff_pos = np.array([
+            [position['x'][i] - position_ref['x']],
+            [position['y'][i] - position_ref['y']],
+            [position['z'][i] - position_ref['z']]
+        ]
+        )
+        d.append(np.sqrt((diff_pos ** 2).sum()))
+
+    valid = np.array(d) < threshold_d
+
+    return valid
+
+
+def plot_cam_mvts(date, angles, position, dates_interp, angles_interp, position_interp, angles_init, position_init,
+                  outdir_cam_mvts):
 
     fig, ax = plt.subplots(3, 2, figsize=(18, 8), sharex=True, tight_layout=True)
-    ax[0, 0].plot(date, angles['yaw'], label = 'yaw (°)')
-    ax[0, 0].plot(dates_interp, angles_interp['yaw'], color='r', label = 'yaw interp (°)')
+    ax[0, 0].axhline(y=angles_init['yaw'], label='yaw init (°)', linewidth=2, color='gold', dashes=(4, 4))
+    ax[0, 0].plot(date, angles['yaw'], label = 'yaw (°)', marker='.', markersize=8)
+    ax[0, 0].plot(dates_interp, angles_interp['yaw'], color='r', label = 'yaw interp (°)', marker='.', markersize=8)
     ax[0, 0].legend(loc='upper right', fontsize=14)
     ax[0, 0].grid(True)
-    ax[1, 0].plot(date, angles['pitch'], label = 'pitch (°)')
-    ax[1, 0].plot(dates_interp, angles_interp['pitch'], color='r', label = 'pitch interp (°)')
+    ax[1, 0].axhline(y=angles_init['pitch'], label='pitch init (°)', linewidth=2, color='gold', dashes=(4, 4))
+    ax[1, 0].plot(date, angles['pitch'], label = 'pitch (°)', marker='.', markersize=8)
+    ax[1, 0].plot(dates_interp, angles_interp['pitch'], color='r', label = 'pitch interp (°)', marker='.', markersize=8)
     ax[1, 0].legend(loc='upper right', fontsize=14)
     ax[1, 0].grid(True)
-    ax[2, 0].plot(date, angles['roll'], label = 'roll (°)')
-    ax[2, 0].plot(dates_interp, angles_interp['roll'], color='r', label='roll interp (°)')
+    ax[2, 0].axhline(y=angles_init['roll'], label='roll init (°)', linewidth=2, color='gold', dashes=(4, 4))
+    ax[2, 0].plot(date, angles['roll'], label = 'roll (°)', marker='.', markersize=8)
+    ax[2, 0].plot(dates_interp, angles_interp['roll'], color='r', label='roll interp (°)', marker='.', markersize=8)
     ax[2, 0].legend(loc='upper right', fontsize=14)
     ax[2, 0].grid(True)
-    ax[0, 1].plot(date, position['x'], label = 'x (m)')
+    ax[0, 1].axhline(y=position_init['x'], label='x init (m)', linewidth=2, color='gold', dashes=(4, 4))
+    ax[0, 1].plot(date, position['x'], label = 'x (m)', marker='.', markersize=8)
+    ax[0, 1].plot(dates_interp, position_interp['x'], color='r', label='x interp (m)', marker='.', markersize=8)
     ax[0, 1].legend(loc='upper right', fontsize=14)
     ax[0, 1].grid(True)
-    ax[1, 1].plot(date, position['y'], label = 'y (m)')
+    ax[1, 1].axhline(y=position_init['y'], label='y init (m)', linewidth=2, color='gold', dashes=(4, 4))
+    ax[1, 1].plot(date, position['y'], label = 'y (m)', marker='.', markersize=8)
+    ax[1, 1].plot(dates_interp, position_interp['y'], color='r', label='y interp (m)', marker='.', markersize=8)
     ax[1, 1].legend(loc='upper right', fontsize=14)
     ax[1, 1].grid(True)
-    ax[2, 1].plot(date, position['z'], label = 'z (m)')
+    ax[2, 1].axhline(y=position_init['z'], label='z init (m)', linewidth=2, color='gold', dashes=(4, 4))
+    ax[2, 1].plot(date, position['z'], label = 'z (m)', marker='.', markersize=8)
+    ax[2, 1].plot(dates_interp, position_interp['z'], color='r', label='z interp (m)', marker='.', markersize=8)
     ax[2, 1].legend(loc='upper right', fontsize=14)
     ax[2, 1].grid(True)
     ax[2, 1].xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m/%d %H'))
@@ -249,18 +321,35 @@ def plot_cam_mvts(date, angles, position, dates_interp, angles_interp, position_
 def run(dir_h, dir_imgs, ref_img_fn, f_gcps, f_cam_params, dir_gcps, odir_cparams_upd, odir_cparams_upd_smooth,
         outdir_cam_mvts):
 
+    # compute camera position of initial georef
+    angles_init, position_init = compute_cam_mvts([Georef.from_param_file(f_cam_params)])
+
     # compute georef parameters for each target image
     date, georef_params_upd = compute_targets_extrinsic(dir_h, f_gcps, f_cam_params, dir_imgs, ref_img_fn, dir_gcps,
                                                   odir_cparams_upd)
 
+    # compute camera movements of each target image
+    angles, position = compute_cam_mvts(georef_params_upd)
+
+    # Despike camera movements
+    valid = despike_cam_mvts(position_init, position)
+    date = np.array(date)[valid]
+    georef_params_upd = np.array(georef_params_upd)[valid]
+    angles['pitch'] = np.array(angles['pitch'])[valid]
+    angles['yaw'] = np.array(angles['yaw'])[valid]
+    angles['roll'] = np.array(angles['roll'])[valid]
+    position['x'] = np.array(position['x'])[valid]
+    position['y'] = np.array(position['y'])[valid]
+    position['z'] = np.array(position['z'])[valid]
+
     # interp extrinsic parameters of target images
     dates_interp, georef_params_interp = interp_targets_extrinsic(date, georef_params_upd, f_cam_params)
-
-    # compute camera movements
-    angles, position = compute_cam_mvts(georef_params_upd)
 
     # compute camera movements interp
     angles_interp, position_interp = compute_cam_mvts(georef_params_interp)
 
     # plot camera movements
-    plot_cam_mvts(date, angles, position, dates_interp, angles_interp, position_interp, outdir_cam_mvts)
+    plot_cam_mvts(date, angles, position,
+                  dates_interp, angles_interp, position_interp,
+                  angles_init, position_init,
+                  outdir_cam_mvts)
